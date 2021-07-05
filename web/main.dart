e8cb40d2d1589e1fe0fb2a779c879f8856aac725
Danil <68150308+mo0nchild@users.noreply.github.com>
2021-07-05 14:57:30 +0300

    Add files via upload

--- web/main.dart
+++ web/main.dart
@@ -5,13 +5,43 @@ import 'package:react/react_dom.dart' as react_dom;
 
 var CoolWidget = react.registerComponent2(() => CoolWidgetComponent());
 
+Map<dynamic, dynamic> styles = {
+  'button': {
+    'border': '2px solid purple',
+    'borderRadius': '10px',
+    'margin': '1rem'
+  },
+  'div': {
+    'border': '2px solid black',
+    'textAlign': 'center',
+    'color': 'purple',
+    'borderRadius': '20px',
+    'margin': '1rem 1rem'
+  }
+};
+
 class CoolWidgetComponent extends react.Component2 {
+  bool State = false;
+
+  void updateText(bool newVal) {
+    State = newVal;
+    forceUpdate();
+  }
+
   @override
   dynamic render() {
     return react.div({
       'className': 'main-widget',
-      'style': {'border': '2px solid black', 'textAlign': 'center'}
-    }, react.p({}, '${props['text']}  --->  ${props['name']}'));
+      'style': styles['div']
+    }, [
+      react.p({'key': '1'}, '${props['text']}  --->  ${props['name']}'),
+      react.h1({'key': '2'}, 'Hello Users!'),
+      react.button({
+        'key': '3',
+        'onClick': (_) => updateText(!State),
+        'style': styles['button']
+      }, State.toString())
+    ]);
   }
 }
 
